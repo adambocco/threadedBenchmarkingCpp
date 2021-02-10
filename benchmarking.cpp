76d78ce3f56d7633385969941dc0f1576afb2700
Adam Bocco <adam.bocco@gmail.com>
2021-02-10 13:56:35 -0500

    Added .gitignore, added command line input options

--- benchmarking.cpp
+++ benchmarking.cpp
@@ -15,15 +15,47 @@
 #include <algorithm>
 #include <thread>
 #include <chrono> 
+#include <sstream>
 #include "chalk.h"
 
 using namespace chalk;
 
 
-void integerOperations(long ops) {for (int i = 0; i < ops; i+=1) {}}
+void integerOperations(long ops) {for (long i = 0; i < ops; i+=1) {}}
 
-void floatingPointOperations(long ops) {for (double i = 0; i < ops; i+=1.0) {}}
+void floatingPointOperations(long ops) {for (double long i = 0; i < ops; i+=1.0) {}}
 
+std::map<long, double> unthreadedTest(std::vector<long> operations, bool iops) {
+
+    std::map<long, double> results;
+
+    for (int i = 0; i < operations.size(); i++) {
+
+// <----------------------------- START TIME -------------------------->
+           std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+// <----------------------------- START TIME -------------------------->
+
+         if (iops) {
+            integerOperations(operations[i]);
+        } 
+        else {
+            floatingPointOperations(operations[i]);
+        }
+
+// <----------------------------- END TIME -------------------------->
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+// <----------------------------- END TIME -------------------------->
+
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+        double totalSeconds = duration/1e6;
+
+        // Append results of time elapsed for all operations to complete
+        results[operations[i]] =  totalSeconds;
+
+    }
+    return results;
+}
 
 // Run threaded tests for all combinations of threads and operations
 std::vector< std::vector< std::vector<double> > > threadedTest(std::vector<long> operations, std::vector<int> threads, bool iops) {
@@ -49,9 +81,11 @@ std::vector< std::vector< std::vector<double> > > threadedTest(std::vector<long>
             
             // Start each thread depending on running FLOPS or IOPS test
             for (int k = 0; k < threads[i]; k++ ) {
+
                 if (iops) {
                     threadList[k] = std::thread(integerOperations, ops);
-                } else {
+                } 
+                else {
                     threadList[k] = std::thread(floatingPointOperations, ops);
                 }
             }
@@ -79,11 +113,15 @@ std::vector< std::vector< std::vector<double> > > threadedTest(std::vector<long>
     return threadedResults;
 }
 
-void writeOutResults(std::vector<int> threads,
-                    std::vector< std::vector< std::vector<double>>>threadedIOPSResults,
-                    std::vector<long long> threadedIOPS, 
-                    std::vector< std::vector< std::vector<double>>>threadedFLOPSResults, 
-                    std::vector<long long> threadedFLOPS) 
+void writeOutResults(std::vector<int> &threads,
+                    std::vector< std::vector< std::vector<double>>> &threadedIOPSResults,
+                    std::vector<long long> &threadedIOPS, 
+                    std::vector< std::vector< std::vector<double>>> &threadedFLOPSResults, 
+                    std::vector<long long> &threadedFLOPS,
+                    std::map<long, double> &unthreadedIOPSResults,
+                    std::map<long, double> &unthreadedFLOPSResults,
+                    long long unthreadedIOPS,
+                    long long unthreadedFLOPS) 
 {
 
     // Setup output 
@@ -122,7 +160,26 @@ void writeOutResults(std::vector<int> threads,
         out << "\t" << std::to_string(threads[i]) << " : ";
         out << threadedFLOPS[i] << ",\n";
     }
-    out << "}\n";
+    out << "}\n\n";
+
+    out << "unthreadedIOPSResults = {\n";
+    for ( const std::pair<const long, double> &entry : unthreadedIOPSResults ) {
+        out << "\t" << std::to_string(entry.first) << " : ";
+        out << std::to_string(entry.second) << ",\n";
+    } 
+    out << "}\n\n";
+
+    out << "unthreadedFLOPSResults = {\n";
+    for ( const std::pair<const long, double> &entry : unthreadedFLOPSResults ) {
+        out << "\t" << std::to_string(entry.first) << " : ";
+        out << std::to_string(entry.second) << ",\n";
+    } 
+    out << "}\n\n";
+
+    out << "unthreadedIOPS = " << std::to_string(unthreadedIOPS);
+    out << "\n\n";
+    out << "unthreadedFLOPS = " << std::to_string(unthreadedFLOPS);
+    out << "\n";
 
     out.close();
 }
@@ -130,28 +187,98 @@ void writeOutResults(std::vector<int> threads,
 int main(int argc, char const *argv[]) {
 
     // Define tests inputs: Number of threads running concurrently / For how many operations
-    std::vector<int> threads = {1, 2, 4, 8};
-    std::vector<long> operations;
-
+    std::vector<int> threads;
+    std::vector<long> operations;   
     long totalOps = 0;
-    for (int i = 1; i < 1000; i++) {
-        operations.push_back(i*1e5);
-        totalOps += (i*1e5);
+
+    // Handle command line arguments
+    bool useDefaults = false;
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+        std::cout << arg << std::endl;
+        if (arg.compare("--default") == 0 || arg.compare("-d")) {
+            useDefaults = true;
+            // Default test values
+            threads = {1, 2, 4, 8};
+            for (int i = 1; i < 5; i+=1) {
+                operations.push_back(i*1e7);
+                totalOps += (i*1e7);
+            }
+        }
     }
+    if (!useDefaults) {
+        std::string userInput = "";
+        std::cout << fg::Blue.Wrap("Enter one or more number of threads to test ( ',' comma delimited; EX: 1,2,4,8 ) :\n");
+        std::cin >> userInput;
+        std::stringstream ss1(userInput);
+
+        for (int i; ss1 >> i;) {
+            threads.push_back(i);    
+            if (ss1.peek() == ',')
+                ss1.ignore();
+        }
+        userInput = "";
+        std::cout << fg::Blue.Wrap("Enter one or more number of operations to test ( ',' comma delimited; EX: 1000,10000,50000,100000 ) :\n");
+        std::cin >> userInput;
+        std::stringstream ss2(userInput);
+        for (long i; ss2 >> i;) {
+            operations.push_back(i);    
+            totalOps += i;
+            if (ss2.peek() == ',')
+                ss2.ignore();
+        }
+    }
+    std::cout << totalOps << std::endl;
 
     // Run threaded tests for IOPS and FLOPS
     std::vector< std::vector< std::vector<double> >> threadedResultsIOPS = threadedTest(operations, threads, true);
     std::vector< std::vector< std::vector<double> >> threadedResultsFLOPS = threadedTest(operations, threads, false);
 
-    std::cout << fmt::Bold.Wrap(fg::Blue.Wrap("\n<------------------Results------------------>\n")) << std::endl;
+    std::map<long, double> unthreadedIOPSResults = unthreadedTest(operations, true);
+    std::map<long, double> unthreadedFLOPSResults = unthreadedTest(operations, false);
 
+
+    long long unthreadedIOPS, unthreadedFLOPS;
+    double sumTime = 0;
     std::vector<double> threadedIOPSTime;
     std::vector<double> threadedFLOPSTime;
+
+    std::cout << fmt::Bold.Wrap(fg::Yellow.Wrap("\n<------------------Detailed Results------------------>\n"));
+    std::cout << fmt::Bold.Wrap(fg::Blue.Wrap("\n<------------------Unthreaded Results------------------>\n"));
+    std::cout << fmt::Bold.Wrap(fg::Green.Wrap("<------------------IOPS------------------>\n"));
+
+    for ( const std::pair<const long, double> &entry : unthreadedIOPSResults ) {
+        
+            std::cout << 
+            fg::Yellow.Wrap(std::to_string(entry.first)) + " operations - "  << 
+            fg::Yellow.Wrap(std::to_string(entry.second)) + " seconds" <<
+            std::endl;
+
+            sumTime += entry.second;
+    } 
+    unthreadedIOPS = totalOps / sumTime;
+    sumTime = 0;
+
+    std::cout << std::endl;
+
+    std::cout << fmt::Bold.Wrap(fg::Green.Wrap("<------------------FLOPS------------------>\n"));
+    for ( const std::pair<const long, double> &entry : unthreadedFLOPSResults ) {
+        
+            std::cout << 
+            fg::Yellow.Wrap(std::to_string(entry.first)) + " operations - "  << 
+            fg::Yellow.Wrap(std::to_string(entry.second)) + " seconds" <<
+            std::endl;
+
+            sumTime += entry.second;
+    } 
+    unthreadedFLOPS = totalOps / sumTime;
+
+    std::cout << fmt::Bold.Wrap(fg::Blue.Wrap("\n<------------------Threaded Results------------------>\n")) << std::endl;
 
     std::cout << fmt::Bold.Wrap(fg::Green.Wrap("<------------------IOPS------------------>")) << std::endl;
     for (int i = 0; i < threadedResultsIOPS.size(); i++) {
         std::cout << fmt::Bold.Wrap(fg::Green.Wrap(std::to_string(threads[i]) + " threads:")) << std::endl;
-        double sumTime = 0;
+        sumTime = 0;
         for (int j = 0; j < threadedResultsIOPS[i].size(); j++) {
 
             std::cout << 
@@ -168,7 +295,7 @@ int main(int argc, char const *argv[]) {
     std::cout << fmt::Bold.Wrap(fg::Green.Wrap("<------------------FLOPS------------------>")) << std::endl;
     for (int i = 0; i < threadedResultsFLOPS.size(); i++) {
         std::cout << fmt::Bold.Wrap(fg::Green.Wrap(std::to_string(threads[i]) + " threads:")) << std::endl;
-        double sumTime = 0;
+        sumTime = 0;
         for (int j = 0; j < threadedResultsFLOPS[i].size(); j++) {
             
             std::cout << 
@@ -184,8 +311,19 @@ int main(int argc, char const *argv[]) {
     std::vector<long long> threadedIOPS;
     std::vector<long long> threadedFLOPS;
 
-    std::cout << fmt::Bold.Wrap(fg::Blue.Wrap("\n<------------------Summary------------------>\n")) << std::endl;
+    std::cout << fmt::Bold.Wrap(fg::Yellow.Wrap("\n<------------------Summary------------------>\n")) << std::endl;
     // Print averages
+    std::cout << fmt::Bold.Wrap(fg::Blue.Wrap("<------------------Unthreaded Results------------------>\n")) << std::endl;
+
+    std::cout << fg::Yellow.Wrap("IOPS: ") << unthreadedIOPS << std::endl;
+    std::cout << fmt::Bold.Wrap(fg::Yellow.Wrap("Giga IOPS: ")) << fmt::Bold.Wrap(std::to_string(unthreadedIOPS/1e9)) << std::endl;
+    std::cout << fg::Yellow.Wrap("FLOPS: ") << unthreadedFLOPS << std::endl;
+    std::cout << fmt::Bold.Wrap(fg::Yellow.Wrap("Giga FLOPS: ")) << fmt::Bold.Wrap(std::to_string(unthreadedFLOPS/1e9)) << std::endl;
+
+
+    std::cout << fmt::Bold.Wrap(fg::Blue.Wrap("\n<------------------Threaded Results------------------>\n")) << std::endl;
+    std::cout << fmt::Bold.Wrap(fg::Green.Wrap("<------------------IOPS------------------>")) << std::endl;
+
     for (int i = 0; i < threadedIOPSTime.size(); i++) {
         long long x = totalOps/threadedIOPSTime[i];
         threadedIOPS.push_back(x);
@@ -193,7 +331,10 @@ int main(int argc, char const *argv[]) {
         std::cout << fg::Yellow.Wrap("IOPS: ") << x << std::endl;
         std::cout << fmt::Bold.Wrap(fg::Yellow.Wrap("Giga IOPS: ")) << fmt::Bold.Wrap(std::to_string(x/1e9)) << std::endl;
     }
+
     std::cout << std::endl;
+    std::cout << fmt::Bold.Wrap(fg::Green.Wrap("<------------------FLOPS------------------>")) << std::endl;
+
     for (int i = 0; i < threadedFLOPSTime.size(); i++) {
         long long x = totalOps/threadedFLOPSTime[i];
         threadedFLOPS.push_back(x);
@@ -202,7 +343,9 @@ int main(int argc, char const *argv[]) {
         std::cout << fmt::Bold.Wrap(fg::Yellow.Wrap("Giga FLOPS: ")) << fmt::Bold.Wrap(std::to_string(x/1e9)) << std::endl;
     }
 
-    writeOutResults(threads, threadedResultsIOPS, threadedIOPS, threadedResultsFLOPS, threadedFLOPS);
+    writeOutResults(threads, threadedResultsIOPS, threadedIOPS,
+                     threadedResultsFLOPS, threadedFLOPS, unthreadedIOPSResults,
+                     unthreadedFLOPSResults, unthreadedIOPS, unthreadedFLOPS);
     return 0;
 }
 
